987bd0ae775b83ea9a0be266591134225201fb65
n1ckn0ck3r <bondarenko.kolya.a@gmail.com>
2024-12-03 20:33:37 +0300

    fifth

--- main.cpp
+++ main.cpp
@@ -2,14 +2,29 @@
 #include <vector>
 #include <algorithm>
 #include <type_traits>
+#include <limits>
+
 template <typename T>
 T maximum(const std::vector<T>& vec) {
     std::vector<T> newVec = vec;
     std::sort(newVec.begin(), newVec.end());
     return newVec.at(newVec.size() - 1);
 }
-int main() {
-    std::vector<int> ints = {4, 1, 2, 7, 23, 1, 90, 0};
+int main() {  
+    int n;
+    while (!(std::cin >> n) || n <= 1) {
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
+    std::vector<int> ints;
+    for (int i = 0; i < n; i++) {
+        int m;
+        while (!(std::cin >> m)) {
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        }
+        ints.push_back(m);
+    }
     std::cout << maximum<int>(ints) << std::endl;
     return 0;
 }
